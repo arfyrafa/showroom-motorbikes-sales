13509a328a798e6771522ed835e891baa03a0f42
Muhammad Arfy Rafa <86044639+AR27idn@users.noreply.github.com>
2026-04-15 23:01:07 +0700

    fix error css

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -143,16 +143,3 @@ const styles = StyleSheet.create({
     paddingBottom: 16,
   },
 });
-
-const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-});
